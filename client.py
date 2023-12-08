06f3398dd75214f9e0616ebcfeb80112f573a283
geeeeenccc <gencgeray@gmail.com>
2023-12-08 23:27:18 +0200

    Update server-client code

--- client.py
+++ client.py
@@ -1,34 +1,66 @@
 import socket
 import numpy as np
+import time
 
+def generate_random_matrices():
+    # Генерація випадкових розмірів та матриць
+    n, m, l = np.random.randint(1000, 2000, size=3)
+    matrix_a = np.random.randint(1, 10, size=(n, m))
+    matrix_b = np.random.randint(1, 10, size=(m, l))
 
-def generate_random_matrix(rows, cols):
-    return np.random.rand(rows, cols)
+    print(f"Згенерована матриця клієнтом:")
+    print("Матриця A:")
+    print(matrix_a)
+    print("Матриця B:")
+    print(matrix_b)
 
+    return n, m, l, matrix_a, matrix_b
 
-def start_client():
-    # Генерація та заповнення матриць
-    matrix_a = generate_random_matrix(np.random.randint(1000, 2000), np.random.randint(1000, 2000))
-    matrix_b = generate_random_matrix(matrix_a.shape[1], np.random.randint(1000, 2000))
+def send_matrices_and_sizes(client, n, m, l, matrix_a, matrix_b):
+    # Надсилання розмірів матриць на сервер
+    print("Надсилання розмірів матриць на сервер")
+    client.send(f"{n},{m},{l}".encode())
 
-    # Встановлення з'єднання з сервером
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 8888))
-
-    # Надсилання розмірів та елементів матриць серверу
-    sizes = f"{matrix_a.shape[0]},{matrix_a.shape[1]},{matrix_b.shape[0]},{matrix_b.shape[1]}"
-    client.send(sizes.encode())
+    # Надсилання матриць на сервер
+    print("Надсилання матриць на сервер")
     client.send(matrix_a.tobytes())
     client.send(matrix_b.tobytes())
 
-    # Отримання та виведення результатів в консоль
-    result_matrix = np.frombuffer(client.recv(matrix_a.shape[0] * matrix_b.shape[1] * 8), dtype=np.float64).reshape(
-        (matrix_a.shape[0], matrix_b.shape[1]))
-    print('[Client] Received result matrix:')
-    print(result_matrix)
+def receive_result_matrix(client, n, l):
+    result_data = b""
+    while True:
+        # Отримання результату від сервера
+        chunk = client.recv(4096)
+        if not chunk:
+            time.sleep(0.1)  # Затримка перед закриттям з'єднання
+            break
+        result_data += chunk
 
-    client.close()
+    if len(result_data) == 0:
+        print("Сервер закрито")
+    else:
+        # Виведення отриманої матриці результату
+        result_matrix = np.frombuffer(result_data, dtype=int).reshape(n, l)
 
+        print("Отримано результат матриці:")
+        print(result_matrix)
+
+def communicate_with_server():
+    try:
+        # Створення сокету та підключення до сервера
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(('127.0.0.1', 8888 ))
+
+        n, m, l, matrix_a, matrix_b = generate_random_matrices()
+
+        send_matrices_and_sizes(client, n, m, l, matrix_a, matrix_b)
+
+        receive_result_matrix(client, n, l)
+
+    finally:
+        # Закриття з'єднання
+        client.close()
+        print("З'єднання закрито")
 
 if __name__ == "__main__":
-    start_client()
+    communicate_with_server()
--- server.py
+++ server.py
@@ -4,33 +4,73 @@ from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 
 
-def handle_client(client_socket):
-    # Отримуємо розміри та елементи матриць від клієнта
-    matrix_sizes = client_socket.recv(1024).decode()
-    rows_a, cols_a, rows_b, cols_b = map(int, matrix_sizes.split(','))
+def receive_matrix_sizes(client_socket):
+    matrix_sizes = client_socket.recv(1024).decode().split(',')
+    return map(int, matrix_sizes)
 
-    matrix_a = np.frombuffer(client_socket.recv(rows_a * cols_a * 8), dtype=np.float64).reshape((rows_a, cols_a))
-    matrix_b = np.frombuffer(client_socket.recv(rows_b * cols_b * 8), dtype=np.float64).reshape((rows_b, cols_b))
 
-    # Обчислюємо результати перемноження матриць
-    result_matrix = np.dot(matrix_a, matrix_b)
+def receive_matrices(client_socket, n, m, l):
+    matrix_a_data = client_socket.recv(n * m * 8)  # Припускаємо float64 для послідовності
+    matrix_b_data = client_socket.recv(m * l * 8)
 
-    # Надсилаємо результати клієнту
-    client_socket.send(result_matrix.tobytes())
-    client_socket.close()
+    if len(matrix_a_data) != n * m * 8 or len(matrix_b_data) != m * l * 8:
+        raise ValueError("Отримано неправильні дані матриці")
+
+    matrix_a = np.frombuffer(matrix_a_data, dtype=float).reshape(n, m)
+    matrix_b = np.frombuffer(matrix_b_data, dtype=float).reshape(m, l)
+
+    return matrix_a, matrix_b
+
+
+def multiply_matrices(matrix_a, matrix_b):
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        result_matrix = executor.submit(np.dot, matrix_a, matrix_b).result()
+    return result_matrix
+
+
+def send_matrix(client_socket, matrix):
+    client_socket.send(matrix.tobytes())
+
+
+def client_handling(client_socket):
+    try:
+        matrix_sizes = receive_matrix_sizes(client_socket)
+        n, m, l = map(int, matrix_sizes)
+
+        if n <= 0 or m <= 0 or l <= 0:
+            raise ValueError("Розміри матриць повинні бути додатніми цілими числами")
+
+        matrix_a, matrix_b = receive_matrices(client_socket, n, m, l)
+
+        print("Від сервера: Отримано матриці:")
+        print("Матриця A:")
+        print(matrix_a)
+        print("Матриця B:")
+        print(matrix_b)
+
+        result_matrix = multiply_matrices(matrix_a, matrix_b)
+
+        print("Надсилаю результати матриці клієнту")
+        send_matrix(client_socket, result_matrix)
+    except ValueError as ve:
+        print(f"Від сервера: Помилка: {ve}")
+    except Exception as e:
+        print(f"Від сервера: Помилка: {e}")
+    finally:
+        client_socket.close()
+        print("Від сервера: З'єднання закрито")
 
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 8888))
+    server.bind(('0.0.0.0', 8888 ))
     server.listen(5)
-    print('[Server] Listening on port 8888...')
+    print("Сервер слухає на порту 8888 ")
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        while True:
-            client, addr = server.accept()
-            print(f'[Server] Accepted connection from {addr}')
-            executor.submit(handle_client, client)
+    while True:
+        client, addr = server.accept()
+        print(f"Від сервера: прийнято з'єднання від {addr}")
+        threading.Thread(target=client_handling, args=(client,)).start()
 
 
 if __name__ == "__main__":
